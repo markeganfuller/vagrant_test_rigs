ace72814e3845a729b0b78d8c2069e3c3822c3c2
Mark Egan-Fuller <mark.eganfuller@bytemark.co.uk>
2020-04-24 15:13:28 +0100

    Update to allow use of libvirt provider.

--- scripts/stretch_raid.sh
+++ scripts/stretch_raid.sh
@@ -4,8 +4,17 @@
 apt-get install mdadm -y
 
 # Partition disks
+if lsblk | grep sda -q; then
+    disk_name=sd
+elif lsblk | grep vda -q; then
+    disk_name=vd
+else
+    echo "Unknown disk naming scheme"
+    exit 1
+fi
+
 for disk in b c d e f g h i j; do
-    sfdisk /dev/sd${disk} << EOF
+    sfdisk /dev/${disk_name}${disk} << EOF
 label: dos
 label-id: 0x6dcc49f5
 device: /dev/sdb
@@ -17,10 +26,10 @@ done
 
 # Create RAID arrays
 # -R needed to skip metadata version warning
-mdadm -C /dev/md0 -l 1 -n 2 /dev/sdb1 /dev/sdc1 -R
-mdadm -C /dev/md1 -l 1 -n 2 -x 1 /dev/sdd1 /dev/sde1 /dev/sdf1 -R
-mdadm -C /dev/md2 -l 1 -n 2 /dev/sdg1 /dev/sdh1 -R
-mdadm -C /dev/md3 -l 1 -n 2 /dev/sdi1 /dev/sdj1 -R
+mdadm -C /dev/md0 -l 1 -n 2 /dev/${disk_name}b1 /dev/${disk_name}c1 -R
+mdadm -C /dev/md1 -l 1 -n 2 -x 1 /dev/${disk_name}d1 /dev/${disk_name}e1 /dev/${disk_name}f1 -R
+mdadm -C /dev/md2 -l 1 -n 2 /dev/${disk_name}g1 /dev/${disk_name}h1 -R
+mdadm -C /dev/md3 -l 1 -n 2 /dev/${disk_name}i1 /dev/${disk_name}j1 -R
 
 for i in {0..3}; do
     # Format RAID arrays
