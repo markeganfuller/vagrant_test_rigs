5cc4ee813aa9885124f50fe166fab5b88f8f99bb
Mark Egan-Fuller <mark.eganfuller@bytemark.co.uk>
2020-11-09 10:09:18 +0000

    Fix RHEL default script and add RHEL OnApp.

---
+++ scripts/rhel/rhel_onapp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rpm -Uvh http://rpm.repo.onapp.com/repo/onapp-repo-6.0.noarch.rpm
+yum install onapp-cp-install –y
+
+# /onapp/onapp-cp-install/onapp-cp-install.sh [-i <ip address>]
+# webinterface admin/changeme
+# License needs to be input
