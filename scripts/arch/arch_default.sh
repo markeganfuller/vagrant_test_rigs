01e699308a1f16eee057cd017a9c8c3fe41d2d93
Mark Egan-Fuller <mark.eganfuller@bytemark.co.uk>
2020-11-16 15:50:07 +0000

    Add dnsutils to arch_default.sh

--- scripts/arch/arch_default.sh
+++ scripts/arch/arch_default.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pacman -Syu --noconfirm
-pacman -S --noconfirm man texinfo bash-completion iproute2 nmap tcpdump vim tree base-devel git net-tools
+pacman -S --noconfirm man texinfo bash-completion iproute2 nmap tcpdump vim tree base-devel git net-tools dnsutils
